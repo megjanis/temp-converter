98d76d9b888587483b2fbfa7aed0f88e02110f25
Meg Janiszewska <mjanis@p1g4.home.lab>
2023-02-13 12:57:55 -0500

    put code into fuction

--- temperature converter.py
+++ temperature converter.py
@@ -1,11 +1,10 @@
-#this is a test
-#test number 2
-celsius = float(input("Enter temperature in Celsius: "))
-fahrenheit = (celsius * 1.8 ) + 32
-print(str(celsius) + " degree Celsius is equal to " + str(fahrenheit ) + " degree Fahrenheit.")
+def temp_converter():
+    celsius = float(input("Enter temperature in Celsius: "))
+    fahrenheit = (celsius * 1.8 ) + 32
+    print(str(celsius) + " degree Celsius is equal to " + str(fahrenheit ) + " degree Fahrenheit.")
 
-fahrenheit = float(input("Enter temperature in Fahrenheit: "))
-celsius = (fahrenheit-32)/1.8
-print(str(fahrenheit) + " degree Fahrenheit is equal to " + str(celsius) + " degree Celsius.")
+    fahrenheit = float(input("Enter temperature in Fahrenheit: "))
+    celsius = (fahrenheit-32)/1.8
+    print(str(fahrenheit) + " degree Fahrenheit is equal to " + str(celsius) + " degree Celsius.")
 
 
